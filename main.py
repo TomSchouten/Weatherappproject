0ae34bea910b391dec4513e99a8e8f5217447c00
tschouten <schoutentom@live.nl>
2022-08-17 13:11:32 +0200

    Deleted API snippet file and moved main code to main.py file.

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-import streamlit as st
+# code snippet weatherAPI
+
 import requests
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.widgets import RadioButtons
+import seaborn
 
-
-st.title("Weather App") # Displays title
-st.subheader("Exploring RapidAPI's weather Endpoints") # Displays subheader
 
 url = "https://weatherapi-com.p.rapidapi.com/current.json"
+
+user_input = input("Enter city: ")
+querystring = {"q": user_input}
 
 headers = {
 	"X-RapidAPI-Key": "5a38b24a20msh5e55cfbff1e7e03p1b44b7jsn91f56f7e209d",
 	"X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
-  }
+}
 
-location = st.text_input("Enter the location", "Chennai")
-querystring = {"q":{location}}
+weather_data = requests.request("GET", url, headers=headers, params=querystring)
 
-response = requests.request("GET", url, headers=headers, params=querystring)
-result = response.text
+weather = weather_data.json()['current']['condition']['text']
+weather_icon = weather_data.json()['current']['condition']['icon']
+temp_c = weather_data.json()['current']['temp_c']
+feel_temp = weather_data.json()['current']['feelslike_c']
+
+
+print(f"The current weather is: {weather}")
+print(f"The current temperature is: {temp_c}")
+print(f"The current temperature feels like: {feel_temp}")
